b9e7ecee2b754087f7abf7381e4ceff530b0a891
ccserm <clcser2021@udec.cl>
2024-11-01 19:58:09 -0300

    Handling input

--- mvirtual.cpp
+++ mvirtual.cpp
@@ -28,4 +28,27 @@ class PageTable{
                 return -1;
             }
         }
+};
+
+int main(int argc, char* argv[]) {
+    if (argc != 7) {
+        cerr << "Uso: " << argv[0] << " -m <num_marcos> -a <algoritmo> -f <archivo_referencias>\n";
+        return 1;
+    }
+
+    int num_marcos = 0;
+    string algoritmo, archivo_referencias;
+
+    for (int i = 1; i < argc; i += 2) {
+        string arg = argv[i];
+        if (arg == "-m") num_marcos = stoi(argv[i + 1]);
+        else if (arg == "-a") algoritmo = argv[i + 1];
+        else if (arg == "-f") archivo_referencias = argv[i + 1];
+        else {
+            cerr << "Uso: " << argv[0] << " -m <num_marcos> -a <algoritmo> -f <archivo_referencias>\n";
+            return 1;
+        }
+    }
+
+    return 0;
 }

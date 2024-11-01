f34efaf29fb3bb07ff9f629361ad640aa77ef737
Tyckong <tocontreras2021@udec.cl>
2024-11-01 19:55:32 -0300

    VMSim class created. Added Constructor method, page reference loader from file method and simulator method.

--- mvirtual.cpp
+++ mvirtual.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include <list>
+#include <queue>
 #include <unordered_map>
 #include <algorithm>
 #include <string>
@@ -28,4 +30,54 @@ class PageTable{
                 return -1;
             }
         }
+}
+
+class VMSim{
+    private:
+        int numMarcos;
+        string algoritmo_de_reemplazo;
+        PageTable pageTable;
+        queue<int> marcos;
+        unordered_map<int, list<int>> marcoMap;
+        int pageErrors = 0;
+        vector<int> pageReferences;
+
+    public:
+        VMSim(int numMarcos, string algoritmo_de_reemplazo) : numMarcos(numMarcos), algoritmo_de_reemplazo(algoritmo_de_reemplazo){}
+
+        void loadReferences(const string &filename){
+            ifstream file(filename);
+            while(file >> page){
+                pageReferences.push_back(page);
+            }
+        }
+
+        void beginSimulation(){
+            if(algoritmo_de_reemplazo == "Optimo"){
+                optimo();
+            }else if(algoritmo_de_reemplazo == "FIFO"){
+                fifo();
+            }else if(algoritmo_de_reemplazo == "LRU"){
+                lru();
+            }else if(algoritmo_de_reemplazo == "LRU Reloj simple"){
+                lru_reloj_simple();
+            }else{
+                cout << "Algoritmo " << algoritmo_de_reemplazo << " no compatible." << endl;
+            }
+        }
+
+        void optimo(){
+
+        }
+
+        void fifo(){
+
+        }
+
+        void lru(){
+
+        }
+
+        void lru_reloj_simple(){
+            
 }
